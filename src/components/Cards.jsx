2e09dea29fb6248cb39e30919ca5b7b7b0f753be
akhil8086 <akhiljith.r@edstem.com>
2023-10-26 11:10:40 +0530

    favourite icon

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,20 +3,21 @@
 
 import React from 'react';
 import './Cards.css'; 
-
+import favouriteIcon from "../assets/favouriteIcon.svg"
 
 
 function Cards({ title, description, imageUrl }) {
   return (
     
-    <div>
     <div className="card">
+    <div className="favorite-icon">
+        <img src={favouriteIcon} alt="Favorite" />
+      </div>
       <div className="card-image" style={{ backgroundImage: `url(${imageUrl})`}}></div>
       <div className="card-content">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
-    </div>
     </div>
   );
 }
